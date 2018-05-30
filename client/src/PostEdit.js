5bf2b5b5d167b1bcffa888e9547e706956e81d27
Karina Li <karina.li@disney.com>
2018-05-30 11:52:43 -0700

    editing post pulls in current time instead of old time

--- client/src/PostEdit.js
+++ client/src/PostEdit.js
@@ -6,18 +6,20 @@ import PostNew from './PostNew';
 class PostEdit extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { post_text: '' };
+    var d = new Date();
+    this.state = {post_title: "", post_text: "", post_time_date: d.toLocaleString()}
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   render() {
-    const {post_title, post_text} = this.state;
+    const {post_title, post_text, post_time_date} = this.state;
 
     return(
       <div className="PostEdit">
         Post Title: <input value={post_title} name="post_title" onChange={this.handleChange}/>
         Post Text: <input value={post_text} name="post_text" onChange={this.handleChange}/>
+        Post Time: <input value={post_time_date} name="post_time_date" onChange={this.handleChange}/>
       <br />
       <button onClick={this.handleSubmit}>Update Post</button>
       </div>
@@ -47,8 +49,8 @@ class PostEdit extends React.Component {
     const {post_id} = this.props.match.params;
     const {user_id} = this.props.match.params;
     axios.get(`/api/users/${user_id}/posts/${post_id}`).then(res => {
-      const {post_title, post_time_date, post_text} = res.data;
-      this.setState({post_title, post_time_date, post_text});
+      const {post_title, post_text} = res.data; //don't need to get post_time_date since updating to current time
+      this.setState({post_title, post_text});
     });
   }
 }
