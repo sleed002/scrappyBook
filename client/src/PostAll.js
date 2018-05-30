741913545d9ed7f37427aac424e597bb7cf90189
Karina Li <karina.li@disney.com>
2018-05-30 16:13:09 -0700

    create all posts page, rendering post text

---
+++ client/src/PostAll.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class AllPosts extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state={
+      posts:[]
+    }
+  }
+
+  render () {
+    const className = "Posts";
+    const { posts } = this.state;
+
+    if (!posts) {
+      return <div className={className}>loading...</div>;
+    }
+
+    return (
+      <div className={className}>
+        {/* <div className='User-Info'>
+          <h3>User Information</h3>
+          <h4>Name: {users.username}</h4>
+          <h4>Nickname: {users.user_nickname}</h4>
+          <h4>Bio: {users.user_bio}</h4>
+          <h4>Favorite Color: {users.user_fave_color}</h4>
+          <h4><img src={users.user_avatar} alt={users.username} /></h4>
+          <div className="Posts">
+            {posts.map(post => this.renderPosts(post))}
+          </div>
+        </div>
+        <button onClick={() => this.handleDelete()}>Delete a user</button>
+        <br/>
+        <Link to={`/users/${users.user_id}/edit`}> Edit </Link>
+        <br/>
+        <Link to={`/users/${users.user_id}/posts/new`}> Create New Entry </Link> */}
+
+        {posts.map(post => this.renderPosts(post))}
+
+      </div>
+    );
+  }
+
+  renderPosts (post) {
+    // let userid = post.user_id
+    // let postid = post.post_id
+    // console.log(postid)
+    return (
+      <div key={post.post_id} className='Post'>
+        <ul>
+          {/* <h3><Link to={`/users/${userid}/posts/${postid}`}>Post Header for post #{postid}!</Link></h3> */}
+          {/* <h3><Link to={`/users/${userid}/posts/${postid}`}>{post.post_title}</Link></h3> */}
+          <p>{post.post_text}</p>
+          {/* {this.state.photos.map(photo => this.renderPhotos(photo, postid))} */}
+        </ul>
+      </div>
+    );
+  }
+
+  // renderPhotos (photo, postid) {
+  //   let photoId = photo.photo_id
+  //   let photoPostId = photo.post_id
+  //   if(postid === photoPostId){
+  //     return (
+  //       photo.photoarr.map(photoUrl => {
+  //         return(
+  //             <img key={photoId} src={photoUrl} height="200px"/>
+  //         )
+  //       })
+  //     );
+  //   }
+  // }
+  //
+  // handleDelete () {
+  //   const { match, history } = this.props;
+  //   const { id } = match.params;
+  //
+  //   axios.delete(`/api/users/${id}`).then(res => {
+  //     history.push('/users'); // another way to redirect (props.history.push)
+  //   });
+  // }
+
+
+  componentDidMount () {
+    axios.get(`/api/posts`).then(res => {
+      this.setState({posts: res.data});
+    });
+  }
+}
+
+export default AllPosts;
---
+++ models/modelsAll.js
@@ -0,0 +1,30 @@
+const db = require('.././db/config'),
+      All = {}; //use object to attach all our query methods
+
+
+//Find all entries for a ALL users
+// All.findAllPostsAllUsers = userId => {
+//   const {id} = userId;
+//   return db.query(`SELECT * FROM posts`, id)
+// };
+All.findAllPostsAllUsers = userId => {
+  const {id} = userId;
+  return db.query(`SELECT posts.post_id, users.user_id, post_text, post_title, array_agg(distinct photo_url) as photoarr, username, user_nickname, user_bio, user_fave_color, user_avatar
+    FROM posts
+    JOIN photos ON posts.post_id = photos.post_id
+    JOIN users ON posts.user_id = users.user_id
+    GROUP BY 1,2,3,4,6,7,8,9,10;`, id)
+};
+
+//Find all photos for ALL posts and users
+//Will return corresponding post & user info
+All.findAllPhotosAllPosts = paramsData => {
+  const {userid, postid} = paramsData;
+  return db.query(`SELECT * from photos
+    JOIN posts
+    ON photos.post_id=posts.post_id
+    JOIN users
+    ON posts.user_id = users.user_id`, [postid, userid])
+};
+
+module.exports = All;
